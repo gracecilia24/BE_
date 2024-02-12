526040d9c3f9fbd58268789dc45091328a47ca94
gracecilia24 <s22110291@student.unklab.ac.id>
2024-02-12 22:19:58 +0800

    menambahkan middleware morgan

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const { data } = require("./member");
+const { users } = require("./user");
+const morgan = require("morgan");
+
+const app = express();
+const port = 3001;
+
+app.use(morgan("combined")); // Menggunakan Morgan middleware untuk logging
+
+app.get("/", (req, res) => {
+  res.send("This is home page");
+});
+
+app.get("/about", (req, res) => {
+  res.json({
+    status: "Success",
+    message: "Response success",
+    description: "Exerscise #3",
+    date: moment(),
+    data,
+  });
+});
+
+app.get("/user", (req, res) => {
+  res.json(users);
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
---
+++ member.js
@@ -0,0 +1,16 @@
+const members = [
+    {
+        id: 1,
+        nim: "12345",
+        nama: "Keren",
+        nomorTelp: "08123456789"        
+    },
+    {
+        id: 2,
+        nim: "67890",
+        nama: "angelli",
+        nomorTelp: "0987654321"
+    }
+];
+
+module.exports = members;
